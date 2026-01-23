1226d2961430148ec29e8d26bf686f80525f31ee
akash-kyadari <akashkyadari1@gmail.com>
2026-01-23 22:08:12 +0530

    coonected auth router to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,18 @@
 import express from "express";
+import authRouter from "./routers/authRouter.js";
+
 const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+// Auth routes
+app.use("/api/auth", authRouter);
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
