e14a4bc49392fd4dbbfb98e56effbec14a77aaeb
akash-kyadari <akashkyadari1@gmail.com>
2026-01-26 20:30:00 +0530

    .

---
+++ backend/src/models/restaurantModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const restaurantSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Link to Restro Owner
+    address: { type: String, required: true },
+    contact: { type: String },
+    photos: [String], // URLs of restaurant photos
+    ratings: { type: Number, default: 0 },
+    reviews: [{ user: String, comment: String, rating: Number }],
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true },
+);
